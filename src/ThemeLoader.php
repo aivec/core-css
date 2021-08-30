6d3592c47c0ed9ab846027b8ac17e9b226d39d0a
Evan Shaw <evandanielshaw@gmail.com>
2021-08-30 11:50:43 +0900

    fix: make ThemeLoader enqueue CSS from dist dir

--- src/ThemeLoader.php
+++ src/ThemeLoader.php
@@ -12,17 +12,22 @@ class ThemeLoader
      *
      * Must be used from within a **theme** directory.
      *
-     * If you are using `coenjacobs/mozart` to namespace composer packages,
-     * make sure this package is not deleted from `vendor` in the build step.
+     * This method assumes that the themes directory exists as
+     * a subdirectory of `ABSPATH` at `/wp-content/themes/`
      *
      * @author Evan D Shaw <evandanielshaw@gmail.com>
      * @return void
      */
     public static function loadCoreCss() {
+        $path = dirname(__FILE__);
         $url = get_stylesheet_directory_uri();
+        $separator = '/wp-content/themes/';
+        $ppieces = explode($separator, $path);
+        $upieces = explode($separator, $url);
+        $url = $upieces[0] . $separator . $ppieces[1];
         wp_enqueue_style(
             'aivec-core-' . Meta::CORE_VERSION,
-            $url . '/vendor/aivec/core-css/src/css/core.css',
+            $url . '/css/core.css',
             [],
             Meta::CORE_VERSION
         );
